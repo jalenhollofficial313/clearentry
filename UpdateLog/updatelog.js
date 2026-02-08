5873558c10f424d7548de0571a90062b46b082d8
jalenhollofficial313 <jalenhollofficial313@gmail.com>
2026-02-07 19:10:26 -0600

    Delete UpdateLog directory

--- UpdateLog/updatelog.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#bar-icon").addEventListener("click", () => {
-    console.log("Check")
-    document.querySelector("#sidebar").style.display = "block";
-  });
-});
